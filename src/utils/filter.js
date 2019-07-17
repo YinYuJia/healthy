ef3ee12517bbe79fdeebaf04836e3d772d7d49d3
unknown <huzh2@epsoft.com.cn>
2019-07-17 10:33:45 +0800

    脱敏修改

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -6,15 +6,17 @@ export function tuoMin(str, front, back) {
             console.log('ruten',ruten)
             return str.replace(ruten, t.substr(0,  1 ))
         }
-        if ( str.length == 2) {
+        else if ( str.length == 2) {
             let t = "************************"
             let ruten = str.substr(0,1); //提取字符串下标之间的字符。
             console.log('ruten2',ruten)
             return str.replace(ruten, t.substr(0, ruten.length < 3 ? 1 : ruten.length))
+        }else{
+            let t = "************************"
+            let ruten = str.substr(front, (str.length - front - back)); //提取字符串下标之间的字符。
+            return str.replace(ruten, t.substr(0, ruten.length < 3 ? 3 : ruten.length)); //字符串中用字符替换另外字符，或替换一个与正则表达式匹配的子串。
         }
-        let t = "************************"
-        let ruten = str.substr(front, (str.length - front - back)); //提取字符串下标之间的字符。
-        return str.replace(ruten, t.substr(0, ruten.length < 3 ? 3 : ruten.length)); //字符串中用字符替换另外字符，或替换一个与正则表达式匹配的子串。
+
     }
     return str;
 }
