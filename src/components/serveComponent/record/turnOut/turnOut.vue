b610f2d4d8dcb8ca4dd8fdcc1497873152589b76
unknown <huzh2@epsoft.com.cn>
2019-07-29 17:23:20 +0800

    出国带药回国日期修改为手动修改

--- src/components/serveComponent/record/turnOut/turnOut.vue
+++ src/components/serveComponent/record/turnOut/turnOut.vue
@@ -51,7 +51,7 @@
                 </div>
                 <div class="InfoLine">
                     <div class="InfoName"><span>结束日期</span></div>
-                    <div class="InfoText"><input @click="openEndPicker" type="text" v-model="form.AAE031" placeholder="请选择" readonly ><svg-icon icon-class="serveComponent_arrowRight"></svg-icon></div>
+                    <div class="InfoText"><input type="text" v-model="form.AAE031" placeholder="请选择" readonly ><svg-icon icon-class="serveComponent_arrowRight"></svg-icon></div>
                 </div>
                 <div class="InfoLine">
                     <div class="InfoName"><span>转出医院</span></div>
