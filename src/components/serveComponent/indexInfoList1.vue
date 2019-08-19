b8ec4dbe5da7415c2daacb138bd8a9d3dd0743c9
yinyujia <6451584@qq.com>
2019-08-19 11:25:39 +0800

    1

--- src/components/serveComponent/indexInfoList1.vue
+++ src/components/serveComponent/indexInfoList1.vue
@@ -460,9 +460,9 @@
                             this.$router.push(route.pop());
                         } else {
                             if (url.indexOf("?") != -1) {
-                                url = url + '&' + this.util.getToken()
+                                url = url + '&' + 'token=' + sessionStorage.getItem("getToken");
                             } else {
-                                url = url + '?' + this.util.getToken()
+                                url = url + '?' + 'token=' + sessionStorage.getItem("getToken");
                             }
                             window.location.href = url;
                         }
