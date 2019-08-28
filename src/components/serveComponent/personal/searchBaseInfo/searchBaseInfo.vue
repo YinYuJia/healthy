7ea55670c15f2426de9b9ce28a73e95ae88b3f6a
zzxprint <zzx971215@163.com>
2019-08-28 16:52:52 +0800

    样式修复

--- src/components/serveComponent/personal/searchBaseInfo/searchBaseInfo.vue
+++ src/components/serveComponent/personal/searchBaseInfo/searchBaseInfo.vue
@@ -152,7 +152,7 @@ export default {
                 display: flex;
                 border-bottom: .01rem solid #D5D5D5;
                 .InfoName{
-                    width: 8rem;
+                    width: 100%;
                     line-height: 1.2rem;
                     text-align: left;
                     span{
