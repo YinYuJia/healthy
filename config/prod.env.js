503328967e4f749081974a1905c33e37c7f57ed7
yinyujia <6451584@qq.com>
2019-09-06 17:45:39 +0800

    1

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,9 +1,9 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  baseURL : '"//ybj.zjzwfw.gov.cn/api"',  //浙理办打包
+  // baseURL : '"//ybj.zjzwfw.gov.cn/api"',  //浙理办打包
   // baseURL : '"/api"',  //开发库
-  // baseURL : '"/api/api"',  //测试库
+  baseURL : '"/api/api"',  //测试库
   
   ifDev:false
 }
--- src/main.js
+++ src/main.js
@@ -145,7 +145,7 @@ console.log('------欢迎进入APP首页main.js---20190715__01---')
 
 
 // 1  网新恩普包  2  浙理办包
-Vue.prototype.$build = "2"
+Vue.prototype.$build = "1"
 
 
 
