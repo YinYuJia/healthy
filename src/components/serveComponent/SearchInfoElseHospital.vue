05cd119aff0db4687b65d0e3f7cf60562cd8159e
huanghz <huanghz@epsoft.com.cn>
2019-09-18 17:36:41 +0800

    异地

--- src/components/serveComponent/SearchInfoElseHospital.vue
+++ src/components/serveComponent/SearchInfoElseHospital.vue
@@ -24,7 +24,7 @@
               <div class="AddressName">{{item.AAE006||"暂无"}}</div>
               <div>
                 <div class="Btn">{{item.AKA101|level()}}</div>
-                <!-- <div class="Btn1" v-if="item.AKB100 != ''">{{item.AKB100|AKB100()}}</div> -->
+                <div class="Btn1" v-if="item.BKB024 != ''">{{item.BKB024|BKB024()}}</div>
                 <!-- <div class="Btn2">{{item.AKB100|AKB100()}}</div> -->
               </div>
             </div>
